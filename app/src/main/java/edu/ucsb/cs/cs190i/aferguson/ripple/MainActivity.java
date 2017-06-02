ce2455db86f97fd873d811107bfd0e2453fcf459
aferguson <drew.t.ferguson@gmail.com>
2017-06-01 22:04:06 -0700

    working authentication, playing CHROMEEEEOOOO

---
+++ app/src/main/java/edu/ucsb/cs/cs190i/aferguson/ripple/MainActivity.java
@@ -0,0 +1,155 @@
+package edu.ucsb.cs.cs190i.aferguson.ripple;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+import com.spotify.sdk.android.player.Spotify;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class MainActivity extends AppCompatActivity implements ConnectionStateCallback, PlayerNotificationCallback {
+
+    //Client ID for individual user (must fetch from spotify)
+    private static final String CLIENT_ID = "5aa6208c6cd54357ad6b55bd67197d51";
+    //Redirect URI after Spotify grants/denies permission (specified on Spotify registration)
+    private static final String REDIRECT_URI = "http://localhost:8888/callback";
+    private static final int REQUEST_CODE = 1337;
+
+    private Player mPlayer;
+
+
+    public void spotifyLoginClicked(View V){
+        final AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+
+        Log.d("MainActivity", "onActivityResult");
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            Log.d("MainActivity", "authResponse:" + response.toString());
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                    mPlayer = Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
+                        @Override
+                        public void onInitialized(Player player) {
+                            mPlayer.addConnectionStateCallback(MainActivity.this);
+                            mPlayer.addPlayerNotificationCallback(MainActivity.this);
+                            mPlayer.play("spotify:track:5bgYTzUDzerRFN7fp86MkQ");
+                        }
+
+                        @Override
+                        public void onError(Throwable throwable) {
+                            Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                        }
+                    });
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+
+
+
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+//        Log.d("MainActivity", "Playback event received: " + playerState.name());
+//        switch (playerState) {
+//            // Handle event type as necessary
+//            default:
+//                break;
+//        }
+        Log.d("MainActivity", "Playback event received: " + eventType.name());
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String errorDetails) {
+//        Log.d("MainActivity", "Playback error received: " + error.name());
+//        switch (error) {
+//            // Handle error type as necessary
+//            default:
+//                break;
+//        }
+        Log.d("MainActivity", "Playback error received: " + errorType.name());
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+        //setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+        //setContentView(R.layout.login_page);
+    }
+
+    @Override
+    public void onLoginFailed(Throwable throwable) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+}
